559dea68f5f7afb1ab67d5fcd0fa9b87e6a73581
FacuMeoni <meonifacu@gmail.com>
2024-10-15 14:53:35 -0300

    feat: implement funcionality to toggle between register and login component

--- client/src/pages/auth_page.jsx
+++ client/src/pages/auth_page.jsx
@@ -1,10 +1,28 @@
-//import RegisterSection from "../components/authComponents/register";
-import LoginSection from "../components/authComponents/login";
+import RegisterSection from "../components/authComponents/register/register";
+import LoginSection from "../components/authComponents/login/login";
+import { useEffect, useState } from "react";
+
 
 function AuthPage () {
+    
+    const[show, setShow] = useState(true);
+
+    useEffect(() => {
+    }, [show])
+
+    function handleClick(){
+        setShow(prevShow => !prevShow)
+    }
+
     return(
         <section>
-            <LoginSection/>
+            <button onClick={handleClick}>
+                { show ? 'Login' : 'Register' }
+            </button>
+            { show 
+                ? <RegisterSection/>
+                : <LoginSection/> 
+            }
         </section>
     )
 }
