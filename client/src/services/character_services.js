2420ac57a6c84ddff254478513f7d5d967bad2b8
FacuMeoni <meonifacu@gmail.com>
2024-10-15 17:21:49 -0300

    feat: implement get all characters functionality

--- client/src/services/character_services.js
+++ client/src/services/character_services.js
@@ -1,5 +1,28 @@
 import axios from 'axios';
+const url_api = 'http://localhost:3000/characters/'
 
-export const getAllCharacters = () => {
-    
+export const getAllCharacters = async() => {
+    try {
+        const { data } = await axios.get(url_api);
+
+        return {
+            sucess: true,
+            characters: data.characters
+        }
+    } catch (error) {
+        if(error.response) { 
+            return error.response.data; 
+        } else if(error.request){
+            return {
+                success: false,
+                message: 'Network error, please try again later.'
+            }
+        } else {
+            console.log('Unexpected error:', error)
+            return {
+                sucess: false,
+                message: 'An unnexpected error ocurred. Please try again later.'
+            }
+        }
+    }
 }
